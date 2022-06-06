a06c9a5083c51941b2130dd607644699222a0339
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-06-06 15:43:24 +0200

    Ajout de la configuration

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const chalk = require('chalk');
+
+const config = require('./config.json');
