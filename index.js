a4e02af12cf92e292714fdc493abeefde0cc879f
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-06-06 15:55:20 +0200

    Modification de index.js

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const io = require('socket.io')(http);
 const chalk = require('chalk');
 const mysql = require('mysql');
 
+const sql = mysql.createConnection(config.sql.authdata);
+
+sql.connect(err => {
+    if (err) {
+        console.error(chalk.red(`Error connecting to database: ${err.stack}`));
+        return;
+    }
+    console.log(chalk.green(`Connected to database: ${config.sql.authdata.database}`));
+});
+
 const log = console.log;
 
 const config = require('./config.json');
