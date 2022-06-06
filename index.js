74880fabebe03e5dd8cc5b45a796f24687732ee4
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-06-06 15:46:40 +0200

    Modif de index.js'

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const chalk = require('chalk');
 
+const log = console.log;
+
 const config = require('./config.json');
+
+app.listen(config.port, () => {
+    log(chalk.green(`Server started on port ${config.port}`));
+});
